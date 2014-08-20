90342e9211ae8795392c1a19022aca7b70cc8fb1
Yihua Guo <yihua@scylla10.research.att.com>
2014-08-20 15:21:38 -0400

    switch to libpcap-1.4.0

--- m.pl
+++ m.pl
@@ -3,4 +3,4 @@ use warnings;
 
 print("compiling ...\n");
 system("rm run");
-system("g++ data.cpp main.cpp tcp_flow.cpp user.cpp util.cpp -Wno-deprecated -Llib -lpcap -I ./libpcap-1.3.0/ -o ./run");
+system("g++ data.cpp main.cpp tcp_flow.cpp user.cpp util.cpp -Wno-deprecated -Llib -lpcap -I ./libpcap-1.4.0/ -o ./run");
